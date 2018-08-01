35bc236ac31fa4545909cb2f7e9b77e4a7af80ca
Claire Harris <wishing.engine@gmail.com>
2018-08-01 15:24:00 +0100

    Tidy up crate, removing unnecessary helper functions

--- src/lib.rs
+++ src/lib.rs
@@ -32,15 +32,25 @@ macro_rules! asset_str {
         )]
         {
             const ASSET: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-            let asset: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Borrowed(ASSET);
-            asset
+            let result: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Borrowed(ASSET);
+
+            result
         }
 
         #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
         {
-            let asset = $crate::load_str(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-            let asset: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Owned(asset);
-            asset
+            let path = concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename);
+
+            let asset = ::std::fs::read_to_string(path).expect(concat!(
+                "Failed to load string from: ",
+                env!("CARGO_MANIFEST_DIR"),
+                "/",
+                $filename
+            ));
+
+            let result: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Owned(asset);
+
+            result
         }
     }};
 }
@@ -74,45 +84,25 @@ macro_rules! asset_bytes {
         {
             const ASSET: &[u8] =
                 include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-            let asset: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Borrowed(ASSET);
-            asset
+            let result: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Borrowed(ASSET);
+
+            result
         }
 
         #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
         {
-            let asset = $crate::load_bytes(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-            let asset: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Owned(asset);
-            asset
+            let path = concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename);
+
+            let asset = ::std::fs::read(path).expect(concat!(
+                "Failed to load bytes from: ",
+                env!("CARGO_MANIFEST_DIR"),
+                "/",
+                $filename
+            ));
+
+            let result: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Owned(asset);
+
+            result
         }
     }};
-}
-
-/// Used internally by [`asset_str!`](macro.asset_str.html) when loading dynamically.
-#[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
-#[inline]
-pub fn load_str(filepath: &str) -> String {
-    use std::fs::File;
-    use std::io::Read;
-
-    let mut file = File::open(filepath)
-        .unwrap_or_else(|e| panic!("Failed to open file `{}`:\n{}", filepath, e));
-    let mut buffer = String::new();
-    file.read_to_string(&mut buffer)
-        .unwrap_or_else(|e| panic!("Failed to read from `{}`:\n{}", filepath, e));
-    buffer
-}
-
-/// Used internally by [`asset_bytes!`](macro.asset_bytes.html) when loading dynamically.
-#[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
-#[inline]
-pub fn load_bytes(filepath: &str) -> Vec<u8> {
-    use std::fs::File;
-    use std::io::Read;
-
-    let mut file = File::open(filepath)
-        .unwrap_or_else(|e| panic!("Failed to open file `{}`:\n{}", filepath, e));
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)
-        .unwrap_or_else(|e| panic!("Failed to read from `{}`:\n{}", filepath, e));
-    buffer
 }
