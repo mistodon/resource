a73ce316f969b659145713e34e6bb106f38ac727
Vi <1149443+mistodon@users.noreply.github.com>
2018-08-01 02:39:10 +0100

    Give crate the ol' one-two

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
-//! This crate contains macros for statically including assets in release mode, but dynamically loading them in debug mode.
+//! This crate contains macros for statically including assets in release mode,
+//! but dynamically loading them in debug mode.
 //!
-//! This is primarily intended for games, allowing you to both avoid file IO in release builds and dynamically reload assets
-//! in debug mode.
+//! This is primarily intended for games, allowing you to both avoid file IO in
+//! release builds and dynamically reload assets in debug mode.
 
 /// Load a text asset statically in release mode, or dynamically in debug.
 ///
 /// The filename is relative to the root of your crate.
 ///
-/// If you wish to override the static/dynamic behaviour, you can use the `force-static` or `force-dynamic`
-/// features.
+/// If you wish to override the static or dynamic behaviour, you can use the
+/// `force-static` or `force-dynamic` features.
 ///
 /// # Panics
 ///
-/// When running in debug mode, this will panic if the file does not exist. In release mode, this
-/// will be a compile error and will never panic.
+/// When dynamically including, this will panic if the file does not exist. When
+/// statically including, this will be a compile error and will never panic.
 ///
 /// # Examples
 ///
@@ -22,39 +23,39 @@
 /// println!("{}", toml);
 /// ```
 #[macro_export]
-macro_rules! asset_str
-{
-    ($filename: tt) => {
+macro_rules! asset_str {
+    ($filename:tt) => {{
+        #[cfg(
+            any(
+                feature = "force-static", all(not(feature = "force-dynamic"), not(debug_assertions))
+            )
+        )]
         {
-            #[cfg(any(feature = "force-static", all(not(feature = "force-dynamic"), not(debug_assertions))))]
-            {
-                const ASSET: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-                let asset: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Borrowed(ASSET);
-                asset
-            }
-
-            #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
-            {
-                let asset = $crate::load_str(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-                let asset: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Owned(asset);
-                asset
-            }
+            const ASSET: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
+            let asset: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Borrowed(ASSET);
+            asset
         }
-    }
-}
 
+        #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
+        {
+            let asset = $crate::load_str(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
+            let asset: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Owned(asset);
+            asset
+        }
+    }};
+}
 
 /// Load a binary asset statically in release mode, or dynamically in debug.
 ///
 /// The filename is relative to the root of your crate.
 ///
-/// If you wish to override the static/dynamic behaviour, you can use the `force-static` or `force-dynamic`
-/// features.
+/// If you wish to override the static or dynamic behaviour, you can use the
+/// `force-static` or `force-dynamic` features.
 ///
 /// # Panics
 ///
-/// When running in debug mode, this will panic if the file does not exist. In release mode, this
-/// will be a compile error and will never panic.
+/// When dynamically including, this will panic if the file does not exist. When
+/// statically including, this will be a compile error and will never panic.
 ///
 /// # Examples
 ///
@@ -63,54 +64,55 @@ macro_rules! asset_str
 /// println!("{:?}", toml_bytes);
 /// ```
 #[macro_export]
-macro_rules! asset_bytes
-{
-    ($filename: tt) => {
+macro_rules! asset_bytes {
+    ($filename:tt) => {{
+        #[cfg(
+            any(
+                feature = "force-static", all(not(feature = "force-dynamic"), not(debug_assertions))
+            )
+        )]
         {
-            #[cfg(any(feature = "force-static", all(not(feature = "force-dynamic"), not(debug_assertions))))]
-            {
-                const ASSET: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-                let asset: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Borrowed(ASSET);
-                asset
-            }
-
-            #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
-            {
-                let asset = $crate::load_bytes(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-                let asset: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Owned(asset);
-                asset
-            }
+            const ASSET: &[u8] =
+                include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
+            let asset: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Borrowed(ASSET);
+            asset
         }
-    }
-}
 
+        #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
+        {
+            let asset = $crate::load_bytes(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
+            let asset: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Owned(asset);
+            asset
+        }
+    }};
+}
 
 /// Used internally by [`asset_str!`](macro.asset_str.html) when loading dynamically.
 #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
 #[inline]
-pub fn load_str(filepath: &str) -> String
-{
+pub fn load_str(filepath: &str) -> String {
     use std::fs::File;
     use std::io::Read;
 
-    let mut file = File::open(filepath).unwrap_or_else(|e| panic!("Failed to open file `{}`:\n{}", filepath, e));
+    let mut file = File::open(filepath)
+        .unwrap_or_else(|e| panic!("Failed to open file `{}`:\n{}", filepath, e));
     let mut buffer = String::new();
-    file.read_to_string(&mut buffer).unwrap_or_else(|e| panic!("Failed to read from `{}`:\n{}", filepath, e));
+    file.read_to_string(&mut buffer)
+        .unwrap_or_else(|e| panic!("Failed to read from `{}`:\n{}", filepath, e));
     buffer
 }
-
 
 /// Used internally by [`asset_bytes!`](macro.asset_bytes.html) when loading dynamically.
 #[cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
 #[inline]
-pub fn load_bytes(filepath: &str) -> Vec<u8>
-{
+pub fn load_bytes(filepath: &str) -> Vec<u8> {
     use std::fs::File;
     use std::io::Read;
 
-    let mut file = File::open(filepath).unwrap_or_else(|e| panic!("Failed to open file `{}`:\n{}", filepath, e));
+    let mut file = File::open(filepath)
+        .unwrap_or_else(|e| panic!("Failed to open file `{}`:\n{}", filepath, e));
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap_or_else(|e| panic!("Failed to read from `{}`:\n{}", filepath, e));
+    file.read_to_end(&mut buffer)
+        .unwrap_or_else(|e| panic!("Failed to read from `{}`:\n{}", filepath, e));
     buffer
 }
-
--- tests/dynamic_tests.rs
+++ tests/dynamic_tests.rs
@@ -1,49 +1,40 @@
 #![cfg(any(feature = "force-dynamic", all(not(feature = "force-static"), debug_assertions)))]
 
-
 #[macro_use]
 extern crate static_assets;
 
-
 #[test]
-fn include_str_asset_dynamic()
-{
+fn include_str_asset_dynamic() {
     let expected = "This\nis\na\nstring\n";
     let included = asset_str!("tests/str.txt");
     assert_eq!(included, expected);
 }
 
 #[test]
-fn included_str_is_owned_dynamic()
-{
+fn included_str_is_owned_dynamic() {
     use std::borrow::Cow;
 
     let included = asset_str!("tests/str.txt");
-    match included
-    {
+    match included {
         Cow::Borrowed(_) => panic!("Included string should be owned dynamically."),
-        Cow::Owned(_) => ()
+        Cow::Owned(_) => (),
     }
 }
 
 #[test]
-fn include_binary_asset_dynamic()
-{
+fn include_binary_asset_dynamic() {
     let expected: &[u8] = &[48, 49, 50, 51, 52];
     let included = asset_bytes!("tests/bytes.bin");
     assert_eq!(included, expected);
 }
 
 #[test]
-fn included_bytes_are_owned_dynamic()
-{
+fn included_bytes_are_owned_dynamic() {
     use std::borrow::Cow;
 
     let included = asset_bytes!("tests/bytes.bin");
-    match included
-    {
+    match included {
         Cow::Borrowed(_) => panic!("Included bytes should be owned dynamically."),
-        Cow::Owned(_) => ()
+        Cow::Owned(_) => (),
     }
 }
-
