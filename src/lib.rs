121d8bdd151c3e3acbfa1fc579a5eb5335c2dd5a
Claire Harris <wishing.engine@gmail.com>
2019-11-05 18:11:46 +0000

    WIP simplify reloading

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 //! `force-static` and `force-dynamic` features.
 //!
 //! ```rust,ignore
+//! use resource::{resource, resource_str};
+//!
 //! // Include text
 //! let readme_text = resource_str!("README.md");
 //!
@@ -25,6 +27,208 @@
 
 #[cfg(all(feature = "force-static", feature = "force-dynamic"))]
 compile_error!("resource: Cannot enable both the force-static and force-dynamic features.");
+
+pub use self::wrapper::Resource;
+
+use std::path::Path;
+
+pub trait ReadFromFile {
+    fn read_from_file(path: &Path) -> Self;
+}
+
+impl ReadFromFile for String {
+    fn read_from_file(path: &Path) -> String {
+        std::fs::read_to_string(path)
+            .map_err(|e| eprintln!("Failed to read `{}` as string: {}", path.display(), e))
+            .unwrap()
+    }
+}
+
+impl ReadFromFile for Vec<u8> {
+    fn read_from_file(path: &Path) -> Vec<u8> {
+        std::fs::read(path)
+            .map_err(|e| eprintln!("Failed to read `{}` as bytes: {}", path.display(), e))
+            .unwrap()
+    }
+}
+
+#[cfg(any(
+    feature = "force-dynamic",
+    all(not(feature = "force-static"), debug_assertions)
+))]
+mod wrapper {
+    use std::{
+        borrow::{Cow, ToOwned},
+        convert::AsRef,
+        ops::Deref,
+        path::{Path, PathBuf},
+        time::SystemTime,
+    };
+
+    use crate::ReadFromFile;
+
+    pub struct Resource<B>(<B as ToOwned>::Owned, PathBuf, SystemTime)
+    where
+        B: 'static + ToOwned + ?Sized;
+
+    impl<B> Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: ReadFromFile,
+    {
+        #[doc(hidden)]
+        pub fn from_file(path: &str) -> Self {
+            let path = PathBuf::from(path);
+            let data = B::Owned::read_from_file(&path);
+            let modified = Self::modified(&path).unwrap_or(SystemTime::UNIX_EPOCH);
+
+            Resource(data, path, modified)
+        }
+
+        fn modified(path: &Path) -> Option<SystemTime> {
+            std::fs::metadata(&path)
+                .and_then(|metadata| metadata.modified())
+                .ok()
+        }
+
+        pub fn changed(&self) -> bool {
+            let modified = Self::modified(&self.1);
+            modified.is_some() && modified != Some(self.2)
+        }
+
+        pub fn reload_if_changed(&mut self) {
+            if self.changed() {
+                self.reload();
+            }
+        }
+
+        pub fn reload(&mut self) {
+            let data = B::Owned::read_from_file(&self.1);
+            let modified = Self::modified(&self.1).unwrap_or(SystemTime::UNIX_EPOCH);
+            self.0 = data;
+            self.2 = modified;
+        }
+    }
+
+    impl<B> AsRef<B> for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: AsRef<B>,
+    {
+        fn as_ref(&self) -> &B {
+            self.0.as_ref()
+        }
+    }
+
+    impl<B> Deref for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: AsRef<B>,
+    {
+        type Target = B;
+
+        fn deref(&self) -> &Self::Target {
+            self.as_ref()
+        }
+    }
+
+    impl<B> Into<Cow<'static, B>> for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+    {
+        fn into(self) -> Cow<'static, B> {
+            Cow::Owned(self.0)
+        }
+    }
+
+    impl<B> Clone for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: Clone,
+    {
+        fn clone(&self) -> Self {
+            Resource(self.0.clone(), self.1.clone(), self.2)
+        }
+    }
+}
+
+#[cfg(any(
+    feature = "force-static",
+    all(not(feature = "force-dynamic"), not(debug_assertions))
+))]
+mod wrapper {
+    use std::{
+        borrow::{Cow, ToOwned},
+        convert::AsRef,
+        ops::Deref,
+    };
+
+    use crate::ReadFromFile;
+
+    pub struct Resource<B>(&'static B)
+    where
+        B: 'static + ToOwned + ?Sized;
+
+    impl<B> Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: ReadFromFile,
+    {
+        #[doc(hidden)]
+        pub fn from_data(data: &'static B) -> Self {
+            Resource(data)
+        }
+
+        pub fn changed(&self) -> bool {
+            false
+        }
+
+        pub fn reload_if_changed(&mut self) {}
+
+        pub fn reload(&mut self) {}
+    }
+
+    impl<B> AsRef<B> for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: AsRef<B>,
+    {
+        fn as_ref(&self) -> &B {
+            self.0
+        }
+    }
+
+    impl<B> Deref for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: AsRef<B>,
+    {
+        type Target = B;
+
+        fn deref(&self) -> &Self::Target {
+            self.0
+        }
+    }
+
+    impl<B> Into<Cow<'static, B>> for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+    {
+        fn into(self) -> Cow<'static, B> {
+            Cow::Borrowed(self.0)
+        }
+    }
+
+    impl<B> Clone for Resource<B>
+    where
+        B: 'static + ToOwned + ?Sized,
+        B::Owned: Clone,
+    {
+        fn clone(&self) -> Self {
+            Resource(self.0)
+        }
+    }
+}
 
 /// Load text resources statically in release mode, or dynamically in debug.
 ///
@@ -60,8 +264,8 @@ compile_error!("resource: Cannot enable both the force-static and force-dynamic 
 /// let (toml, lib) = resource_str!(("Cargo.toml", "src/lib.rs"));
 /// let as_array = resource_str!(["Cargo.toml", "src/lib.rs"]);
 ///
-/// assert_eq!(toml, as_array[0]);
-/// assert_eq!(lib, as_array[1]);
+/// assert_eq!(toml.as_ref(), as_array[0].as_ref());
+/// assert_eq!(lib.as_ref(), as_array[1].as_ref());
 /// ```
 ///
 /// Load multiple text files and apply a transformation to each one:
@@ -81,11 +285,11 @@ compile_error!("resource: Cannot enable both the force-static and force-dynamic 
 #[macro_export]
 macro_rules! resource_str {
     ([ $($filenames:tt),* $(,)* ], $load_fn:expr) => {
-        [ $($load_fn(resource_str!($filenames).as_ref())),* ]
+        [ $(resource_str!($filenames, $load_fn)),* ]
     };
 
     (( $($filenames:tt),* $(,)* ), $load_fn:expr) => {
-        ( $($load_fn(resource_str!($filenames).as_ref())),* )
+        ( $(resource_str!($filenames, $load_fn)),* )
     };
 
     ([ $($filenames:tt),* $(,)* ]) => {
@@ -97,24 +301,13 @@ macro_rules! resource_str {
     };
 
     ($filename:tt, $load_fn:expr) => {
-        $load_fn(resource_str!($filename).as_ref())
+        $load_fn(
+            <$crate::Resource<str> as std::convert::AsRef<str>>::as_ref(&resource_str!($filename))
+        )
     };
 
     ($filename:tt) => {
-        {
-            let path = concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename);
-
-            let resource = ::std::fs::read_to_string(path).expect(concat!(
-                "Failed to load string from: ",
-                env!("CARGO_MANIFEST_DIR"),
-                "/",
-                $filename
-            ));
-
-            let result: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Owned(resource);
-
-            result
-        }
+        $crate::Resource::<str>::from_file(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename))
     };
 }
 
@@ -125,11 +318,11 @@ macro_rules! resource_str {
 #[macro_export]
 macro_rules! resource_str {
     ([ $($filenames:tt),* $(,)* ], $load_fn:expr) => {
-        [ $($load_fn(resource_str!($filenames).as_ref())),* ]
+        [ $(resource_str!($filenames, $load_fn)),* ]
     };
 
     (( $($filenames:tt),* $(,)* ), $load_fn:expr) => {
-        ( $($load_fn(resource_str!($filenames).as_ref())),* )
+        ( $(resource_str!($filenames, $load_fn)),* )
     };
 
     ([ $($filenames:tt),* $(,)* ]) => {
@@ -141,16 +334,13 @@ macro_rules! resource_str {
     };
 
     ($filename:tt, $load_fn:expr) => {
-        $load_fn(resource_str!($filename).as_ref())
+        $load_fn(
+            <$crate::Resource<str> as std::convert::AsRef<str>>::as_ref(&resource_str!($filename))
+        )
     };
 
     ($filename:tt) => {
-        {
-            const RESOURCE: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-            let result: ::std::borrow::Cow<'static, str> = ::std::borrow::Cow::Borrowed(RESOURCE);
-
-            result
-        }
+        $crate::Resource::<str>::from_data(include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename)))
     };
 }
 
@@ -189,8 +379,8 @@ macro_rules! resource_str {
 /// let (toml, lib) = resource!(("Cargo.toml", "src/lib.rs"));
 /// let as_array = resource!(["Cargo.toml", "src/lib.rs"]);
 ///
-/// assert_eq!(toml, as_array[0]);
-/// assert_eq!(lib, as_array[1]);
+/// assert_eq!(toml.as_ref(), as_array[0].as_ref());
+/// assert_eq!(lib.as_ref(), as_array[1].as_ref());
 /// ```
 ///
 /// Load binary files and apply a transformation to each one:
@@ -211,11 +401,11 @@ macro_rules! resource_str {
 #[macro_export]
 macro_rules! resource {
     ([ $($filenames:tt),* $(,)* ], $load_fn:expr) => {
-        [ $($load_fn(resource!($filenames).as_ref())),* ]
+        [ $(resource!($filenames, $load_fn)),* ]
     };
 
     (( $($filenames:tt),* $(,)* ), $load_fn:expr) => {
-        ( $($load_fn(resource!($filenames).as_ref())),* )
+        ( $(resource!($filenames, $load_fn)),* )
     };
 
     ([ $($filenames:tt),* $(,)* ]) => {
@@ -227,24 +417,13 @@ macro_rules! resource {
     };
 
     ($filename:tt, $load_fn:expr) => {
-        $load_fn(resource!($filename).as_ref())
+        $load_fn(
+            <$crate::Resource<[u8]> as std::convert::AsRef<[u8]>>::as_ref(&resource!($filename))
+        )
     };
 
     ($filename:tt) => {
-        {
-            let path = concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename);
-
-            let resource = ::std::fs::read(path).expect(concat!(
-                "Failed to load bytes from: ",
-                env!("CARGO_MANIFEST_DIR"),
-                "/",
-                $filename
-            ));
-
-            let result: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Owned(resource);
-
-            result
-        }
+        $crate::Resource::<[u8]>::from_file(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename))
     };
 }
 
@@ -255,11 +434,11 @@ macro_rules! resource {
 #[macro_export]
 macro_rules! resource {
     ([ $($filenames:tt),* $(,)* ], $load_fn:expr) => {
-        [ $($load_fn(resource!($filenames).as_ref())),* ]
+        [ $(resource!($filenames, $load_fn)),* ]
     };
 
     (( $($filenames:tt),* $(,)* ), $load_fn:expr) => {
-        ( $($load_fn(resource!($filenames).as_ref())),* )
+        ( $(resource!($filenames, $load_fn)),* )
     };
 
     ([ $($filenames:tt),* $(,)* ]) => {
@@ -271,16 +450,222 @@ macro_rules! resource {
     };
 
     ($filename:tt, $load_fn:expr) => {
-        $load_fn(resource!($filename).as_ref())
+        $load_fn(
+            <$crate::Resource<[u8]> as std::convert::AsRef<[u8]>>::as_ref(&resource!($filename))
+        )
     };
 
     ($filename:tt) => {
-        {
-            const RESOURCE: &[u8] =
-                include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename));
-            let result: ::std::borrow::Cow<'static, [u8]> = ::std::borrow::Cow::Borrowed(RESOURCE);
-
-            result
-        }
+        $crate::Resource::<[u8]>::from_data(include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/", $filename)))
     };
+}
+
+#[cfg(test)]
+mod single_file_transform_tests {
+    fn rev_string(string: &str) -> String {
+        string.chars().rev().collect()
+    }
+
+    fn rev_bytes(bytes: &[u8]) -> Vec<u8> {
+        bytes.iter().cloned().rev().collect()
+    }
+
+    #[test]
+    fn transform_single_string() {
+        let s = resource_str!("tests/str.txt", rev_string);
+        assert_eq!(s, "\ngnirts\na\nsi\nsihT");
+    }
+
+    #[test]
+    fn transform_single_bytes() {
+        let s = resource!("tests/bytes.bin", rev_bytes);
+        assert_eq!(s, &[52, 51, 50, 49, 48]);
+    }
+}
+
+#[cfg(test)]
+mod multi_file_tests {
+    fn rev_string(string: &str) -> String {
+        string.chars().rev().collect()
+    }
+
+    fn rev_bytes(bytes: &[u8]) -> Vec<u8> {
+        bytes.iter().cloned().rev().collect()
+    }
+
+    #[test]
+    fn load_array_of_multiple_strings() {
+        let [a, b, c] = resource_str!([
+            "tests/string_a.txt",
+            "tests/string_b.txt",
+            "tests/string_c.txt",
+        ]);
+
+        assert_eq!([&*a, &*b, &*c], ["String A\n", "String B\n", "String C\n"]);
+    }
+
+    #[test]
+    fn load_tuple_of_multiple_strings() {
+        let (a, b, c) = resource_str!((
+            "tests/string_a.txt",
+            "tests/string_b.txt",
+            "tests/string_c.txt",
+        ));
+
+        assert_eq!(&*a, "String A\n");
+        assert_eq!(&*b, "String B\n");
+        assert_eq!(&*c, "String C\n");
+    }
+
+    #[test]
+    fn load_array_of_multiple_bytes() {
+        let [a, b, c] = resource!([
+            "tests/bytes_a.bin",
+            "tests/bytes_b.bin",
+            "tests/bytes_c.bin",
+        ]);
+
+        assert_eq!(
+            [&*a, &*b, &*c],
+            [
+                b"Bytes A".as_ref(),
+                b"Bytes B".as_ref(),
+                b"Bytes C".as_ref()
+            ]
+        );
+    }
+
+    #[test]
+    fn load_tuple_of_multiple_bytes() {
+        let (a, b, c) = resource!((
+            "tests/bytes_a.bin",
+            "tests/bytes_b.bin",
+            "tests/bytes_c.bin",
+        ));
+
+        assert_eq!(&*a, b"Bytes A".as_ref());
+        assert_eq!(&*b, b"Bytes B".as_ref());
+        assert_eq!(&*c, b"Bytes C".as_ref());
+    }
+
+    #[test]
+    fn load_with_fn_array_of_multiple_strings() {
+        let [a, b, c] = resource_str!(
+            [
+                "tests/string_a.txt",
+                "tests/string_b.txt",
+                "tests/string_c.txt",
+            ],
+            rev_string
+        );
+
+        assert_eq!([a, b, c], ["\nA gnirtS", "\nB gnirtS", "\nC gnirtS"]);
+    }
+
+    #[test]
+    fn load_with_fn_tuple_of_multiple_strings() {
+        let (a, b, c) = resource_str!(
+            (
+                "tests/string_a.txt",
+                "tests/string_b.txt",
+                "tests/string_c.txt",
+            ),
+            rev_string
+        );
+
+        assert_eq!(a, "\nA gnirtS");
+        assert_eq!(b, "\nB gnirtS");
+        assert_eq!(c, "\nC gnirtS");
+    }
+
+    #[test]
+    fn load_with_fn_array_of_multiple_bytes() {
+        let [a, b, c] = resource!(
+            [
+                "tests/bytes_a.bin",
+                "tests/bytes_b.bin",
+                "tests/bytes_c.bin",
+            ],
+            rev_bytes
+        );
+
+        assert_eq!(
+            [a, b, c],
+            [
+                b"A setyB".as_ref(),
+                b"B setyB".as_ref(),
+                b"C setyB".as_ref()
+            ]
+        );
+    }
+
+    #[test]
+    fn load_with_fn_tuple_of_multiple_bytes() {
+        let (a, b, c) = resource!(
+            (
+                "tests/bytes_a.bin",
+                "tests/bytes_b.bin",
+                "tests/bytes_c.bin",
+            ),
+            rev_bytes
+        );
+
+        assert_eq!(a, b"A setyB".as_ref());
+        assert_eq!(b, b"B setyB".as_ref());
+        assert_eq!(c, b"C setyB".as_ref());
+    }
+}
+
+#[cfg(test)]
+#[cfg(any(
+    feature = "force-dynamic",
+    all(not(feature = "force-static"), debug_assertions)
+))]
+mod dynamic_tests {
+    use super::*;
+
+    use std::borrow::Cow;
+
+    #[test]
+    fn str_dynamic() {
+        match resource_str!("tests/str.txt").into() {
+            Cow::Owned(ref s) if s == "This\nis\na\nstring\n" => (),
+            _ => panic!("Expected owned string!"),
+        }
+    }
+
+    #[test]
+    fn bytes_dynamic() {
+        match resource!("tests/bytes.bin").into() {
+            Cow::Owned(ref s) if s == &[48, 49, 50, 51, 52] => (),
+            _ => panic!("Expected owned bytes!"),
+        }
+    }
+}
+
+#[cfg(test)]
+#[cfg(any(
+    feature = "force-static",
+    all(not(feature = "force-dynamic"), not(debug_assertions))
+))]
+mod static_tests {
+    use super::*;
+
+    use std::borrow::Cow;
+
+    #[test]
+    fn str_static() {
+        match resource_str!("tests/str.txt").into() {
+            Cow::Borrowed(s) if s == "This\nis\na\nstring\n" => (),
+            _ => panic!("Expected borrowed string!"),
+        }
+    }
+
+    #[test]
+    fn bytes_static() {
+        match resource!("tests/bytes.bin").into() {
+            Cow::Borrowed(s) if s == &[48, 49, 50, 51, 52] => (),
+            _ => panic!("Expected borrowed bytes!"),
+        }
+    }
 }
